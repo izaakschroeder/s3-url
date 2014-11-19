2382147dc2c3988cc84c518be299c2631a400857
Izaak Schroeder <izaak.schroeder@gmail.com>
2014-11-19 00:10:01 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# s3-url
+
+Go between URLs and AWS options for S3.
+
+![build status](http://img.shields.io/travis/izaakschroeder/s3-url.svg?style=flat)
+![coverage](http://img.shields.io/coveralls/izaakschroeder/s3-url.svg?style=flat)
+![license](http://img.shields.io/npm/l/s3-url.svg?style=flat)
+![version](http://img.shields.io/npm/v/s3-url.svg?style=flat)
+![downloads](http://img.shields.io/npm/dm/s3-url.svg?style=flat)
+
+Sometimes you just need to convert an `s3://bucket/key` to something you can feed into AWS. This library has you covered.
+
+```javascript
+var s3Url = require('s3-url');
+s3Url.optionsToURL({ Bucket: 'bucket', Key: 'key' });
+```
+
+```javascript
+var s3Url = require('s3-url');
+s3Url.urlToOptions('s3://bucket/key');
+```
---
+++ test/spec/s3-url.spec.js
@@ -0,0 +1,56 @@
+
+'use strict';
+
+var s3Url = require('s3-url'),
+	url = require('url');
+
+describe('optionsToURL', function() {
+	it('should fail on non-objects', function() {
+		expect(s3Url.optionsToUrl).to.throw(TypeError);
+	});
+
+	it('should work', function() {
+		var result = s3Url.optionsToUrl({
+			Bucket: 'bucket',
+			Key: 'key'
+		});
+		expect(result).to.equal('s3://bucket/key');
+	});
+
+	it('should work with just a bucket', function() {
+		var result = s3Url.optionsToUrl({
+			Bucket: 'bucket'
+		});
+		expect(result).to.equal('s3://bucket');
+	});
+});
+
+describe('urlToOptions', function() {
+
+	it('should work with something from url.parse', function() {
+		expect(s3Url.urlToOptions(url.parse('s3://bucket/key')))
+			.to.deep.equal({
+				Bucket: 'bucket',
+				Key: '/key'
+			});
+	});
+
+	it('should work with strings', function() {
+		expect(s3Url.urlToOptions('s3://bucket/key'))
+			.to.deep.equal({
+				Bucket: 'bucket',
+				Key: '/key'
+			});
+	});
+
+	it('should work with just a bucket', function() {
+		expect(s3Url.urlToOptions('s3://bucket'))
+			.to.deep.equal({
+				Bucket: 'bucket'
+			});
+	});
+
+	it('should fail with nothing', function() {
+		expect(s3Url.urlToOptions).to.throw(TypeError);
+	});
+});
